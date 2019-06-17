60d451692c9a2fbcf2b01dbc6dc4012d7166227e
HMPNK <51913753+HMPNK@users.noreply.github.com>
2019-06-17 15:34:51 +0200

    Update 02_ORDERCONTIGS.pl

--- INSTALL/02_ORDERCONTIGS.pl
+++ INSTALL/02_ORDERCONTIGS.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-#CSA version 2.6 (Chromosome Scale Assembler, employing synteny with diverged reference genomes)
+#CSA version 2.6c (Chromosome Scale Assembler, employing synteny with diverged reference genomes)
 
 #THIS SCRIPT RUNS WHOLE GENOME ALIGNMENTS AND ORDERS CONTIGS
 
@@ -27,7 +27,7 @@ getopts("o:g:t:c:x:", \%options);
 
 if(! $ARGV[0] && ! $options{c})       {
        print STDERR "
-CSA version 2.6 (Chromosome Scale Assembler, employing synteny with diverged reference genomes)
+CSA version 2.6c (Chromosome Scale Assembler, employing synteny with diverged reference genomes)
 Author: Heiner Kuhl, Phd (kuhl\@igb-berlin.de)
 
 THIS SCRIPT RUNS: assembly comparisons and contig ordering
