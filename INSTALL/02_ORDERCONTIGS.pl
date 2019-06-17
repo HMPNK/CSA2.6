58b17ded323dd57ed4a8f8f7ce7db152252250a3
HMPNK <51913753+HMPNK@users.noreply.github.com>
2019-06-17 21:31:33 +0200

    Update 02_ORDERCONTIGS.pl

--- INSTALL/02_ORDERCONTIGS.pl
+++ INSTALL/02_ORDERCONTIGS.pl
@@ -99,7 +99,7 @@ rm used$c.list unused$c.list
 			}
 $COMMAND="${COMMAND}ln -s $out.scf$c.fa $out.step2.fa\n\n#END of CTGORDER\n
 echo;echo PRIMARY SCAFFOLD STATS:
-$bin/seq_n50.pl $out.step2.fa
+perl $bin/seq_n50.pl $out.step2.fa
 echo;date;echo FINISHED CSA STEP2;echo
 ";
 print "\n$COMMAND";
